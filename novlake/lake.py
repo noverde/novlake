555e1a8cbac27107bcfb069ea7473124cc4e4cb7
Pierre-Marie Leveque <pierre@noverde.com.br>
2019-09-10 15:29:16 -0300

    Improve user config

--- novlake/lake.py
+++ novlake/lake.py
@@ -1,42 +1,67 @@
 import datetime as dt
 import os
-from dotenv import load_dotenv
-
 import subprocess
 import boto3
+import re
+import yaml
 
 import awswrangler
+from urllib.parse import urlparse
 
 # The easiest and most common usage consists on calling
 # load_dotenv when the application starts, which will load
 # environment variables from a file named .env in the current
 # directory or any of its parents or from the path specificied
-load_dotenv()
-
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 class Lake():
-    def __init__(self):
+    def __init__(self, user_name):
+        self.user_name = user_name
         self.s3 = boto3.client('s3')
+
+        NOVLAKE_SETTINGS = os.getenv("NOVLAKE_SETTINGS")
+
+        if not NOVLAKE_SETTINGS:
+            raise Exception("Missing NOVLAKE_SETTINGS environment variable")
+
+        settings = urlparse(NOVLAKE_SETTINGS)
+        response = self.s3.get_object(Bucket=settings.netloc, Key=settings.path.lstrip('/'))
+
+        try:
+            config = yaml.safe_load(response["Body"])
+        except yaml.YAMLError as exc:
+            raise exc
+
+        if not user_name in config["users"]:
+            print(config)
+            raise Exception("Unknown user")
+
+        user_config = config["users"][user_name]
+
+        self.notebook_path = user_config["notebook_path"]        
+        self.athena_schema_name = user_config["athena_schema_name"]
+        self.s3_repo = user_config["s3_repo"]
+        self.athena_output = user_config["athena_output"]
+
+        self.documentation_home = config["documentation_home"]
+
         self.session = awswrangler.Session()
-        self.user_name = os.getenv("NOVLAKE_USERNAME")
-        self.data_bucket = os.getenv("NOVLAKE_DATA_BUCKET")
-        self.notebook_bucket = os.getenv("NOVLAKE_NOTEBOOK_BUCKET")
-        self.athena_output = os.getenv("NOVLAKE_ATHENA_OUTPUT")
-        self.datastore_schema = os.getenv("NOVLAKE_DATASTORE_SCHEMA")
-        self.documentation_home = os.getenv("NOVLAKE_DOCUMENTATION_HOME")
+        # self.datastore = self.refresh_datastore()
 
-        self.datastore = self.refresh_datastore()
+    # def refresh_datastore(self):
+    #     """Read datastore config file and returns it as a dict"""
 
-    def refresh_datastore(self):
-        """Read datastore config file and returns it as a dict"""
-
-        return dict()
+    #     return dict()
 
     def query(self, query, database="default"):
         """Queries data using Athena and returns pandas dataframe"""
 
-        if "limit" not in query or "LIMIT not in query":
+        if not re.findall(r"limit", query, re.I):
             raise Exception("Use LIMIT in your query")
+
+        if not self.athena_output:
+            raise Exception("Missing NOVLAKE_ATHENA_OUTPUT environment variable")
             
         return self.session.pandas.read_sql_athena(
             sql=query,
--- tests/test_novlake.py
+++ tests/test_novlake.py
@@ -1,5 +1,13 @@
 from novlake import __version__
-
+from novlake.lake import Lake
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_lake_init():
+    lake = Lake(user_name="pierre")
+
+    df = lake.query("select id from dumps.loans LIMIT 10")
+
+    assert len(df) == 10
